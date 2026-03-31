f0b7966058766a8dbad0be0da67dc70606ef5e28
ankurpandey12091 <ankurpandey28july@gmail.com>
2026-04-01 01:37:16 +0530

    java program

---
+++ program.cc
@@ -0,0 +1,9 @@
+// multiply by 2 by taking input
+#include <iostream>
+int main() {
+    int x;
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    std::cout << "The result is: " << x * 2 << std::endl;
+    return 0 ;
+}
