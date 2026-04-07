1daa69d3addcd1da17aee8ae9fa2cb9f36194d1e
ankurpandey12091 <ankurpandey28july@gmail.com>
2026-04-08 00:27:52 +0530

    array

---
+++ array.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+int i ;
+int arr[10]={2,3,4,5,7,7};
+for (i=0;i<10;i++)
+{
+    cout<<arr[i]<<" " ;
+}
+
+    return 0;
+}
--- program.cc
+++
@@ -1,9 +0,0 @@
-// multiply by 2 by taking input
-#include <iostream>
-int main() {
-    int x;
-    std::cout << "Enter a number: ";
-    std::cin >> x;
-    std::cout << "The result is: " << x * 2 << std::endl;
-    return 0 ;
-}
