2f0a6e98efd49ed2bdcde8798b87072b8fef5f17
ankurpandey12091 <ankurpandey28july@gmail.com>
2026-04-08 21:58:44 +0530

    "msg"

---
+++ Linersearch.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+int i ;
+int arr[10]={2,3,4,5,7,7};
+int key=5;
+int flag=0;
+int size=10;
+for (i=0;i<size;i++)
+{
+    if (arr[i==key])
+    {
+        flag==1;
+        break;
+    }
